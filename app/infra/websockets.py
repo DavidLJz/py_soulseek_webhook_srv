ed27d0d642ab3cf35ad8d36396116a085a5904c1
DavidLJz <4dlj1995@gmail.com>
2025-03-17 01:07:28 -0600

    refactor ConnectionManager to use client IDs for active connections and add event listener support

--- app/infra/websockets.py
+++ app/infra/websockets.py
@@ -1,17 +1,45 @@
+from typing import Callable
+from asyncio import iscoroutinefunction
 from starlette.websockets import WebSocket
 
 # https://fastapi.tiangolo.com/advanced/websockets/#handling-disconnections-and-multiple-clients
 
 class ConnectionManager:
     def __init__(self):
-        self.active_connections: list[WebSocket] = []
+        self.active_connections: dict[str, WebSocket] = {}
+        
+        self._event_handlers : dict[str, list[Callable]] = {
+            'connection': []
+        }
 
-    async def connect(self, websocket: WebSocket):
+    async def _emit_events(self, event:str, *args, **kwargs):
+        listeners = self._event_handlers.get(event, None)
+
+        for listener in listeners:
+            try:
+                if iscoroutinefunction(listener):
+                    await listener(*args, **kwargs)
+                else:
+                    listener(*args, **kwargs)
+
+            except Exception:
+                print(
+                    "exception notifying listener %r of event %r" % listener, event
+                )
+
+    async def register_connection_event_listener(self, listener: Callable[ [str, WebSocket], None ]):
+        self._event_handlers.setdefault('connection', [])
+
+        self._event_handlers['connection'].append(listener)
+
+    async def connect(self, client_id:str, websocket: WebSocket):
         await websocket.accept()
-        self.active_connections.append(websocket)
+        self.active_connections[client_id] = websocket
 
-    async def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
+        self._emit_events('connection', client_id, websocket)
+
+    async def disconnect(self, client_id:str, websocket: WebSocket):
+        self.active_connections.pop(client_id)
         await websocket.close()
 
     async def send_personal_message(self, message: str, websocket: WebSocket):
@@ -22,5 +50,7 @@ class ConnectionManager:
             await connection.send_text(message)
 
     async def disconnect_all(self):
-        for connection in self.active_connections:
+        for connection in self.active_connections.values():
             await connection.close()
+
+        self.active_connections.clear()
